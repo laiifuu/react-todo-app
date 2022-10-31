aebb708962b23eea39f3cd6f9aed26aed2bffd0d
Hayet Fellah <hayet.fellah145@gmail.com>
2022-10-31 10:59:41 +0100

    Create the necessary components

---
+++ src/components/TodosList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+
+
+export default class TodosList extends React.Component {
+  
+  render() {
+    return (
+      <ul>
+        {this.props.todos.map(todo => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    )
+  }
+}
