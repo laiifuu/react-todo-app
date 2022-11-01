c190c70d472f1de15b4eddcad739c7d838799ed8
Hayet Fellah <hayet.fellah145@gmail.com>
2022-11-01 21:00:15 +0100

    Create Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <header className="header">
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+    </nav>
     <h1>todos</h1>
   </header>
 );
